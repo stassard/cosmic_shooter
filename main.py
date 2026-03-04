423d78b7c913404521a42592e0ecb867eb564abf
stanislav.sardarian <stanislavsardarian@larixon.com>
2026-03-04 17:26:59 +0100

    feat: alien movement acceleration and a score counter have been added.

--- main.py
+++ main.py
@@ -13,26 +13,28 @@ screen = pygame.display.set_mode((screen_width, screen_height))
 
 pygame.display.set_caption("Cosmic Shooter")
 
-FIGHTER_STEP = 1
+FIGHTER_STEP = 2
 fighter_image = pygame.image.load("images/fighter.png")
 fighter_width, fighter_height = fighter_image.get_size()
 fighter_x, fighter_y = screen_width / 2 - fighter_width / 2, screen_height - fighter_height
 fighter_is_moving_left, fighter_is_moving_right = False, False
 
 
-BALL_STEP = 0.3
+BALL_STEP = 1
 rocket_image = pygame.image.load("images/rocket.png")
 rocket_width, rocket_height = rocket_image.get_size()
 rocket_x, rocket_y = 0, 0
 rocket_was_fired = False
 
 ALIEN_STEP = 1
+alien_speed = ALIEN_STEP
 alien_image = pygame.image.load("images/alien.png")
 alien_width, alien_height = alien_image.get_size()
 alien_x, alien_y = randint(0, screen_width - alien_width), 0
 
 
 game_is_running = True
+game_score = 0
 
 
 while game_is_running:
@@ -60,7 +62,7 @@ while game_is_running:
     if fighter_is_moving_right and fighter_x <= screen_width - fighter_width - FIGHTER_STEP:
         fighter_x += FIGHTER_STEP
 
-    alien_y += ALIEN_STEP
+    alien_y += alien_speed
 
     if rocket_was_fired and rocket_y + rocket_height < 0:
         rocket_was_fired = False
@@ -77,6 +79,9 @@ while game_is_running:
     if rocket_was_fired:
         screen.blit(rocket_image, (rocket_x, rocket_y))
 
+    game_score_text = game_font.render("Score: " + str(game_score), True, "white")
+    screen.blit(game_score_text, (20,20))
+
     pygame.display.update()
 
     if alien_y + alien_height > fighter_y:
@@ -87,6 +92,8 @@ while game_is_running:
             alien_y < rocket_y < alien_y + alien_height - rocket_height):
         rocket_was_fired = False
         alien_x, alien_y = randint(0, screen_width - alien_width), 0
+        alien_speed += ALIEN_STEP / 2
+        game_score += 1
 
 
 game_over_text = game_font.render("GAME OVER", True, "white")
